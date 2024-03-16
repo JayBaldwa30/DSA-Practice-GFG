9c307ae26e9a369bbd149ae6af3045b03197b752
jaybaldwa30 <jaybaldwa123@gmail.com>
2024-03-16 14:44:14 +0530

    to calculate sum of digits of a number

---
+++ Recursion/sumofDigits.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int sumofDigitsofNumber(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    return sumofDigitsofNumber(n / 10) + n % 10;
+}
+s
+int main()
+{
+    int n = 10101;
+    cout << sumofDigitsofNumber(n);
+    return 0;
+}
